f59f51c058213e4b15836c22a7666005866c2f8d
Your Name <you@example.com>
2024-10-29 11:03:17 +0100

    test des node.js skripts

---
+++ calc.js
@@ -0,0 +1,10 @@
+function add(a, b){
+    return a + b;
+}
+
+const num1 = 1;
+const num2 = 3;
+
+const result = add (num1, num2);
+
+console.log("Ergebnis: " + num1 + " + " + num2 + "=" + result );
